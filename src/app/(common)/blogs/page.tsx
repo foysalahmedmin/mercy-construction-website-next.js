6c3ee43bc2288e71c3b43f18ec3c5d9b7f6b8da7
foysalahmedmin <foysalahmedmin@gmail.com>
2025-04-12 04:12:44 +0600

    blogs page

--- src/app/(common)/blogs/page.tsx
+++ src/app/(common)/blogs/page.tsx
@@ -1,29 +1,47 @@
 import { blogs } from "@/assets/data/blogs";
-import { Button } from "@/components/ui/Button";
-import { Description, SectionTitle, Title } from "@/components/ui/SectionTitle";
+import PageHeaderSection from "@/components/partials/Sections/PageHeaderSection";
 import Link from "next/link";
 
-const BlogsSection = () => {
+const BlogsPage = () => {
   return (
-    <section className="py-16 md:py-24">
-      <div className="container">
-        <div className="grid gap-8 md:gap-14 lg:grid-cols-3">
-          <div className="space-y-8 md:space-y-12">
-            <SectionTitle className="max-w-60">
-              <Title>Blog</Title>
-              <Description>
-                Our people are dedicated to finding solutions to every
-                challenge. That spirit makes for some great stories.
-              </Description>
-            </SectionTitle>
-            <Link href={"/blogs"} className="inline-block">
-              <Button asChild={true} size="lg">
-                <span>VIEW ALL BLOGS</span>
-              </Button>
-            </Link>
+    <main>
+      <PageHeaderSection
+        subtitle="Insights"
+        title="Blogs"
+        description="Our people are dedicated to finding solutions to every challenge. That spirit makes for some great stories."
+      />
+      <section className="py-16 md:py-24">
+        <div className="container">
+          <div className="bg-muted">
+            <div className="group grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
+              <div className="bg-muted flex flex-col justify-center space-y-8 self-stretch p-8 md:space-y-12 md:p-12 md:px-24">
+                <h3 className="text-4xl">
+                  The Ultimate Guide to Choosing Durable and Stylish Stucco
+                  Finishes
+                </h3>
+                <p className="font-light">
+                  In design, sustainability, and programming, the Boston
+                  University Center for Computing & Data Sciences marks the
+                  future of construction in higher education. The 19-story,
+                  350,000-square-foot building is a testament to the
+                  university's commitment to innovation and excellence.
+                </p>
+              </div>
+              <div className="aspect-[5/4] w-full">
+                <img
+                  className="size-full object-cover object-center"
+                  src="/images/partials/blog-thumbnail.png"
+                  alt="blog thumbnail"
+                />
+              </div>
+            </div>
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:col-span-2">
-            {blogs?.slice(0, 2)?.map((blog, index) => (
+        </div>
+      </section>
+      <section className="py-16 md:py-24">
+        <div className="container">
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-2 md:gap-12 lg:col-span-3">
+            {blogs?.map((blog, index) => (
               <div
                 key={blog._id}
                 className="basis-1/1 sm:basis-1/2 sm:px-4 md:basis-1/3 md:px-6"
@@ -74,9 +92,9 @@ const BlogsSection = () => {
             ))}
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </main>
   );
 };
 
-export default BlogsSection;
+export default BlogsPage;
