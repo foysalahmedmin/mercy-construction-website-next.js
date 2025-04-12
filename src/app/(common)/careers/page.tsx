3a40c71de1b149efbc7c26ac6a11df11cd2d77e4
foysalahmedmin <foysalahmedmin@gmail.com>
2025-04-12 11:39:29 +0600

    career page

--- src/app/(common)/careers/page.tsx
+++ src/app/(common)/careers/page.tsx
@@ -1,5 +1,7 @@
+import BenefitsSection from "@/components/partials/Sections/BenefitsSection";
 import FollowUpSection from "@/components/partials/Sections/FollowUpSection";
 import PageHeaderSection from "@/components/partials/Sections/PageHeaderSection";
+import SplitSection2 from "@/components/partials/Sections/SplitSection2";
 
 const CareersPage = () => {
   return (
@@ -15,6 +17,15 @@ const CareersPage = () => {
         title="Life at Mercy Construction"
         description="At Mercy Construction, being an employee means being part of a dedicated family that thrives on collaboration and shared success. Our culture is built on mutual respect and support, where every team member is encouraged to bring their unique talents to the table. We believe in empowering our employees to take ownership of their work, fostering a sense of pride in every project we undertake. Together, we celebrate our diverse backgrounds and experiences, driving innovation and excellence in the construction industry. Here, your contributions matter, and your growth is our priority. Join us, and be part of a team that values integrity, quality, and the pursuit of excellence."
         image="/images/(careers-page)/career-life.png"
+      />
+      <BenefitsSection />
+      <SplitSection2
+        className="bg-muted"
+        subtitle="Growing"
+        title="Intern Program"
+        description="At Mercy Construction, our Internship Program is a vital part of nurturing the next generation of industry leaders. With an impressive 85% of our former interns advancing their careers within the company, they are actively contributing to shaping our future across all levels. Our interns gain hands-on experience by collaborating with seasoned professionals on various project phases, allowing them to develop practical skills and industry insights. We offer internships tailored for students aspiring to become Project Managers, Superintendents, and other key roles in areas such as marketing, finance, information technology, and safety. With opportunities for increased responsibility throughout multiple internship sessions, Mercy Construction is dedicated to empowering interns to grow their careers while making meaningful contributions to our projects and culture."
+        image1="/images/(careers-page)/internship-1.png"
+        image2="/images/(careers-page)/internship-2.png"
       />
       <PageHeaderSection
         subtitle="Career Opportunities"
---
+++ src/assets/data/employee-benefits.ts
@@ -0,0 +1,53 @@
+import {
+  CalendarClock,
+  GraduationCap,
+  HeartHandshake,
+  Palmtree,
+  Stethoscope,
+  Trophy,
+} from "lucide-react";
+
+export const employee_benefits = [
+  {
+    _id: "350db34",
+    title: "Mental Wellness Support",
+    description:
+      "Access to mental health resources and programs to promote emotional well-being and resilience.",
+    icon: HeartHandshake,
+  },
+  {
+    _id: "576f6ab",
+    title: "Comprehensive Health Benefits",
+    description:
+      "Comprehensive medical, dental, and vision coverage to ensure the well-being of you and your family.",
+    icon: Stethoscope,
+  },
+  {
+    _id: "367003c",
+    title: "Flexible Work Arrangements",
+    description:
+      "Options for remote work and flexible hours to help you maintain a healthy work-life balance.",
+    icon: CalendarClock,
+  },
+  {
+    _id: "4f1b2f7",
+    title: "Professional Development Opportunities",
+    description:
+      "Ongoing training and education programs to enhance your skills and advance your career.",
+    icon: GraduationCap,
+  },
+  {
+    _id: "dc5c301",
+    title: "Generous Paid Time Off",
+    description:
+      "Ample vacation and paid time off to recharge and maintain a healthy work-life balance.",
+    icon: Palmtree,
+  },
+  {
+    _id: "75282bd",
+    title: "Employee Recognition Programs",
+    description:
+      "Regular recognition and rewards for outstanding performance, fostering a culture of appreciation and motivation.",
+    icon: Trophy,
+  },
+];
